5e563db3f168952e56c50164a2202b6d087f3d40
Yogensia <yogensia@gmail.com>
2019-12-19 17:21:11 +0100

    Update !help command to show a simple embed link to COMMANDS.md

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,15 +1,16 @@
-const common = require('../utils/common')
-
 /**
  * COMMAND: !help
  *
- * Show a link to command list in documentation.
+ * Shows a link to command list in documentation.
  */
 exports.run = (client, msg, args) => {
-  const emote_angry = common.showEmote('Angry', client)
-  const emote_peepoPants = common.showEmote('peepoPants', client)
-
-  msg.author.send(`You can use this DM to test commands on your own if you want! ${emote_peepoPants}`)
-  msg.channel.send(`Help!? ${emote_angry} Ok, ${msg.author.username}, here's a link with a list of commands!
-https://github.com/yogensia/korean-lady-discord-bot/blob/master/README.md`)
+  // Send documentation link in a nice and clean embed.
+  msg.channel.send({
+    embed: {
+      color: 3447003,
+      title: 'KoreanLady Command Documentation',
+      url: 'https://github.com/yogensia/korean-lady-discord-bot/blob/master/COMMANDS.md#koreanlady-discord-bot',
+      description: 'You will find all the commands available and their usage by following this link!\nYou can also DM me to test commands!'
+    }
+  })
 }
