1fb57f8c8c5fa36d5b6b46108b11592615e4b77c
Yogensia <yogensia@gmail.com>
2020-01-07 02:48:22 +0100

    Add !slot command

---
+++ src/commands/slots.js
@@ -0,0 +1,40 @@
+const common = require('../utils/common')
+const math = require('../utils/math')
+const random = require('../utils/random')
+
+const run = (client, msg, args) => {
+  // Random symbols.
+  const symbol_array = [
+    'cherries',
+    'watermelon',
+    'pineapple',
+    'broccoli',
+  ]
+
+  const result = []
+  result[0] = math.getRandomStringFromArray(symbol_array)
+  result[1] = math.getRandomStringFromArray(symbol_array)
+  result[2] = math.getRandomStringFromArray(symbol_array)
+  const symbols = `:${result[0]}: :${result[1]}: :${result[2]}:`
+
+  // Random exclamation.
+  const exclamation = random.exclamation()
+
+  // Compare values and send message.
+  if (math.areEqual(result)) {
+    const emote = common.getCustomEmote(client, 'Hypers')
+    msg.channel.send(`${symbols} **${exclamation} You won!** ${emote}`)
+  } else {
+    const emote = common.getCustomEmote(client, 'sadcat')
+    msg.channel.send(`${symbols} Sorry, no prize... ${emote}`)
+  }
+}
+
+module.exports = {
+  name: 'slots',
+  desc: 'Try your luck at the slot machines. There are 3 reels with 4 possible symbols, so you have a 1 in 63 chance to win!',
+  aliases: [ 'slot', 'slotmachine'],
+  usage: ['slots'],
+  examples: ['slots'],
+  run
+}
