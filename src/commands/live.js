6390af5ec2abb78f30f54cd73f4c233ed9f19f33
Yogensia <yogensia@gmail.com>
2020-01-07 02:45:46 +0100

    Make !live command usable by server owner only

--- src/commands/live.js
+++ src/commands/live.js
@@ -1,32 +1,37 @@
 const common = require('../utils/common')
-const twitch = require('../utils/twitch')
 const random = require('../utils/random')
+const twitch = require('../utils/twitch')
 
 const run = (client, msg, args) => {
-  // Request stream info from Twitch API.
-  twitch.request('streams?user_login=apollolol')
-    .then((stream) => {
-      // Random data.
-      const sticker = random.sticker()
-      const exclamation = random.exclamation()
+  // Guild owner only command.
+  if (msg.guild.ownerID === msg.author.id) {
+    // Request stream info from Twitch API.
+    twitch.request('streams?user_login=apollolol')
+      .then((stream) => {
+        // Random data.
+        const sticker = random.sticker()
+        const exclamation = random.exclamation()
 
-      // Send an embed message with the stream details.
-      msg.channel.send({
-        embed: {
-          color: 3447003,
-          title: stream.title,
-          description: `**${exclamation} Apollo is going live!**`,
-          url: `https://twitch.tv/${stream.user_name.toLowerCase()}`,
-          thumbnail: {
-            url: sticker,
+        // Send an embed message with the stream details.
+        msg.channel.send({
+          embed: {
+            color: 3447003,
+            title: stream.title,
+            description: `**${exclamation} Apollo is going live!**`,
+            url: `https://twitch.tv/${stream.user_name.toLowerCase()}`,
+            thumbnail: {
+              url: sticker,
+            }
           }
-        }
+        })
       })
-    })
-    .catch((error) => {
-      // Send an embed message with the error.
-      common.sendExceptionMsg(msg, error)
-    })
+      .catch((error) => {
+        // Send an embed message with the error.
+        common.sendExceptionMsg(msg, error)
+      })
+  } else {
+    common.sendErrorMsg(msg, `Sorry, only the server owner can use this command.`)
+  }
 }
 
 module.exports = {
