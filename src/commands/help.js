239e839ba8e5caf9e776fd1ee1162ae54e04ff6a
Yogensia <yogensia@gmail.com>
2020-01-09 12:50:56 +0100

    Add !list command and update !help command to match

--- src/commands/help.js
+++ src/commands/help.js
@@ -70,7 +70,10 @@ const run = (client, msg, args) => {
         color: 3447003,
         title: 'KoreanLady Commands',
         url: 'https://github.com/yogensia/korean-lady-discord-bot/blob/master/COMMANDS.md#koreanlady-discord-bot',
-        description: 'Here you will find all commands available and their usage!\nYou can also DM me to test commands!'
+        description: 'Here you will find all commands available and their usage!',
+        footer: {
+          text: 'Tip: You can also DM me to test commands!'
+        }
       }
     })
   }
---
+++ src/commands/list.js
@@ -0,0 +1,22 @@
+const run = (client, msg, args) => {
+  // Send general documentation link in a nice and clean embed.
+  msg.channel.send({
+    embed: {
+      color: 3447003,
+      title: 'KoreanLady Commands',
+      url: 'https://github.com/yogensia/korean-lady-discord-bot/blob/master/COMMANDS.md#koreanlady-discord-bot',
+      description: 'Here you will find all commands available and their usage!',
+      footer: {
+        text: 'Tip: You can also DM me to test commands!'
+      }
+    }
+  })
+}
+
+module.exports = {
+  name: 'list',
+  desc: 'Lists all available commands.',
+  aliases: ['commands'],
+  usage: ['list'],
+  run
+}
