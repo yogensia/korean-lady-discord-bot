adc2ae7d058ec6153e783305dcdb0334b9aedd13
Yogensia <yogensia@gmail.com>
2020-07-13 02:52:05 +0200

    Adjust !fight command for slightly shorter fights

--- src/commands/fight.js
+++ src/commands/fight.js
@@ -10,51 +10,44 @@ const attacks = [
   // Name, damage, accuracy, crit chance, heal.
 
   // Weak attacks (2x).
-  ['Acrobatics', 25, 95, 70, 0],
-  ['Bite', 20, 100, 80, 0],
-  ['Butt-Stomp', 15, 95, 85, 0],
-  ['First Try', 30, 70, 75, 0],
-  ['Glitterdust', 25, 80, 75, 0],
-  ['Fracture', 30, 75, 70, 0],
-  ['Nether Blade', 20, 90, 80, 0],
-  ['Tantrum', 15, 70, 90, 0],
-
-  ['Friendliness Pellet', 15, 95, 85, 0],
-  ['Splishy Splash', 30, 70, 75, 0],
-  ['Flying Nachos', 25, 80, 75, 0],
-  ['Judgamental Look', 30, 75, 70, 0],
-  ['Tickle Attack', 20, 90, 80, 0],
-  ['Harsh Language', 15, 70, 90, 0],
+  ['Acrobatics', 45, 95, 70, 0],
+  ['Bite', 40, 100, 80, 0],
+  ['Butt-Stomp', 35, 95, 85, 0],
+  ['First Try', 50, 70, 75, 0],
+  ['Glitterdust', 45, 80, 75, 0],
+  ['Fracture', 50, 75, 70, 0],
+  ['Nether Blade', 40, 90, 80, 0],
+  ['Tantrum', 35, 70, 90, 0],
+  ['Friendliness Pellet', 35, 95, 85, 0],
+  ['Splishy Splash', 50, 70, 75, 0],
+  ['Flying Nachos', 45, 80, 75, 0],
+  ['Judgamental Look', 50, 75, 70, 0],
+  ['Tickle Attack', 40, 90, 80, 0],
+  ['Harsh Language', 35, 70, 90, 0],
 
   // Normal attacks (1x).
   ['Falcon Punch', 50, 90, 50, 0],
   ['Piercing Arrow', 45, 85, 65, 0],
   ['Savagery', 60, 80, 50, 0],
   ['Frozen Orb', 55, 85, 60, 0],
-
   ['Backhand Chop', 45, 85, 65, 0],
   ['Soul Crush', 60, 80, 50, 0],
   ['Calamity Cannon', 55, 75, 50, 0],
 
   // Powerful attacks (1x).
-  ['10M Volt Thunderbolt', 80, 80, 15, 0],
-  ['Holy Hand Grenade', 100, 65, 30, 0],
-  ['Hadouken', 75, 85, 20, 0],
-  ['Slipper', 60, 95, 45, 0],
-
-  ['Hundred Rending Fists', 75, 85, 25, 0],
-  ['Super Combo', 75, 85, 20, 0],
-  ['Fus-Ro-Dah', 60, 95, 45, 0],
+  ['10M Volt Thunderbolt', 50, 80, 15, 0],
+  ['Holy Hand Grenade', 80, 75, 20, 0],
+  ['Hadouken', 55, 85, 20, 0],
+  ['Slipper', 50, 95, 45, 0],
+  ['Hundred Rending Fists', 55, 85, 25, 0],
+  ['Super Combo', 55, 85, 20, 0],
+  ['Fus-Ro-Dah', 40, 95, 45, 0],
 
   // Defensive moves (1x).
-  ['Vampiric Touch', 0, 90, 80, 30],
-  ['Apply Band-Aid', 0, 95, 70, 25],
-  ['Drink your Water', 0, 100, 75, 15],
-  ['Pineapple Pizza', 0, 90, 80, 30],
-
-  ['Eat Your Greens', 0, 95, 70, 25],
-  ['Rember Happy Day', 0, 100, 75, 15],
-  ['Praise the Sun', 0, 90, 80, 30]
+  ['Drink your Water', 0, 80, 75, 15],
+  ['Pineapple Pizza', 0, 70, 80, 30],
+  ['Eat Your Greens', 0, 75, 70, 25],
+  ['Praise the Sun', 0, 70, 80, 30]
 ]
 
 /**
@@ -68,7 +61,7 @@ const getAttack = () => {
     dmg: parseInt((attackData[1] * math.getRandomFloat(1, 1.3)).toFixed(0), 10),
     heal: parseInt((attackData[4] * math.getRandomFloat(0.75, 1.25)).toFixed(0), 10),
     crit: false,
-    miss: attackData[2] < math.getRandomFloat(1, 100)
+    miss: attackData[2] < math.getRandomFloat(-50, 100)
   }
 
   // Roll crit chance.
