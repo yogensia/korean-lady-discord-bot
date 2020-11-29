ad28cdc7e7d8660079b7b22dba8ab632af2405e4
Yogensia <yogensia@gmail.com>
2020-11-29 20:51:41 +0100

    Add shuffleArray() function to random utilities

--- src/utils/random.js
+++ src/utils/random.js
@@ -69,8 +69,36 @@ const exclamationNegative = () => {
   return math.getRandomStringFromArray(array)
 }
 
+/**
+ * Randomizes the order of items in an array.
+ *
+ * https://stackoverflow.com/a/2450976
+ *
+ * @return {string} A random url.
+ */
+const shuffleArray = (array) => {
+  let currentIndex = array.length
+  let temporaryValue
+  let randomIndex
+
+  // While there remain elements to shuffle...
+  while (currentIndex !== 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex -= 1
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex]
+    array[currentIndex] = array[randomIndex]
+    array[randomIndex] = temporaryValue
+  }
+
+  return array
+}
+
 module.exports = {
   exclamation,
   exclamationNegative,
+  shuffleArray,
   sticker
 }
